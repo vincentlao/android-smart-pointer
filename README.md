0dca101cec7a2a7bd5a711ee8261164c5a8dbe89
Lzhiyong <975434530@qq.com>
2018-07-16 10:57:01 +0800

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# android-smart-pointer
-smartPointer
+## android-smart-pointer
+Android智能指针，强指针，弱指针，轻量级智能指针使用，其实很简单，就几个头文和一个cpp源文件，这是Android自己实现的一套用于内存管理的解决方案，如果你不想使用C++11提供的智能指针，这是一个很好的选择，当然了智能指针也是有缺陷的，毕竟它是对象，会占用更多的内存，同时运行效率也没有普通指针高！
+
+这些源文件在android-8.1/system/core/libutils下，有兴趣的可以自己看看源码，写得很精妙！
+
+##### 构建
+```
+mkdir build
+cd build
+cmake ..
+#或者
+clang++ -Iinclude -o main main.cpp RefBase.cpp
+```
+
---
+++ main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+#include "utils/RefBase.h"
+
+using namespace std;
+using namespace android;
+
+// 安卓智能指针的使用
+class Person : public RefBase {
+
+  private:
+    string name;
+    //定义A B两个人
+    wp<Person> A;
+    wp<Person> B;
+    weakref_type *weak;
+
+  public:
+    Person(string name = "") {
+        // 构造函数
+        this->name = name;
+        cout << "Pserson(" << name << ") 构造函数调用" << endl;
+    }
+
+    virtual ~Person() {
+        /*
+         * 析构函数，这里的析构函数可以定义成虚析构函数
+         * 也可以定义成普通的析构函数
+         * 如果Person是作为基类的话就一定要定义成虚析构函数
+         */
+        cout << this->name << "~Person() 析构函数调用" << endl;
+        
+    }
+
+    void setA(sp<Person> &A) {
+        this->A = A;
+        weak = A->getWeakRefs();
+        cout << "A的弱引用计数：" << weak->getWeakCount() << endl;
+    }
+
+    void setB(sp<Person> &B) {
+        this->B = B;
+        weak = B->getWeakRefs();
+        cout << "B的弱引用计数：" << weak->getWeakCount() << endl;
+    }
+
+    string getName(void) { return name; }
+
+    void printInfo(void) {
+        // 把A B强制升级为强引用
+        // 因为弱引用只记录了对象的引用计数，不能访问类的成员属性和函数
+        sp<Person> a = A.promote();
+        sp<Person> b = B.promote();
+
+        // 如果对象被销毁了，就不能升级为强引用 promote函数就会返回NULL
+        if (a != NULL) {
+            cout << "A:  " << a->getName() << endl;
+            cout << "A的强引用计数：" << a->getStrongCount() << endl;
+        }
+
+        if (b != NULL) {
+            cout << "B:  " << b->getName() << endl;
+            cout << "B的强引用计数：" << b->getStrongCount() << endl;
+        }
+    }
+};
+
+void test_func() {
+    // 注意这里不要写成  sp<Person> *A = new Person()
+    // 也不要写成 sp<Person> A = new sp<Person>();
+
+    // 可以写成sp<Person> A(new Person())这种写法是对的
+    // A本身是一个对象，只是重载了->操作符，这个重载函数返回一个Person的指针对象
+    // 所以才可以以指针方式调用Person的成员函数，A->printInfo
+    sp<Person> A = new Person("张三");
+    sp<Person> B = new Person("李四");
+    cout << "#################################" << endl;
+    
+    // 这里设置A B两个对象相互引用
+    // 如果在这里使用轻量级LightRefBase智能指针，test_func函数在调用返回后，A B两个对象都不会被自动删除
+    // 对于对象之间的相互引用，只能使用强弱指针的方式
+    A->setB(B);
+    B->setA(A);
+
+    A->printInfo();
+    B->printInfo();
+
+    cout << "#################################" << endl;
+}
+
+int main(int argc, char **argv) {
+    test_func();
+    return 0;
+}
